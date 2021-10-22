d35bc62a4532c6fbecb9d480a043f3f38c15d53c
Bishnu Nath <bishnu1324@gmail.com>
2021-10-22 14:31:59 +0600

    33-3 International Travel, Display countries name, capital

---
+++ js/countries.js
@@ -0,0 +1,23 @@
+const loadCountries = () => {
+  fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = counties => {
+  // for (const country of counties) {
+  //     console.log(country);
+  // }
+  const countriesDiv = document.getElementById('countries');
+  counties.forEach(country => {
+      // console.log(country);
+      const div = document.createElement('div');
+      div.classList.add('country')
+      div.innerHTML = `
+          <h3>${country.name.common}</h3>
+          <p>${country.capital}</p>
+      `;
+      countriesDiv.appendChild(div);
+  });
+}
